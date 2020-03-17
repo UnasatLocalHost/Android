a92935e3b6d0180a2f56351c069dc2ef107bd91c
UnasatLocalHost <50671395+UnasatLocalHost@users.noreply.github.com>
2020-03-17 16:59:41 -0300

    User can now open camera and take photo.

--- app/src/main/java/sr/unasat/bproduct/fragments/FragmentOne.java
+++ app/src/main/java/sr/unasat/bproduct/fragments/FragmentOne.java
@@ -1,12 +1,16 @@
 package sr.unasat.bproduct.fragments;
 
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 
 import android.view.View;
@@ -17,10 +21,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
-import java.util.List;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
 
 import sr.unasat.bproduct.R;
-import sr.unasat.bproduct.afterlogin.MainActivity;
 
 import static android.app.Activity.RESULT_OK;
 
@@ -31,6 +34,10 @@ public class FragmentOne extends Fragment {
     private Button captureImage, detectImage;
     private ImageView imageView;
     private TextView textView;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+
+
 
 
 
@@ -53,20 +60,48 @@ public class FragmentOne extends Fragment {
         captureImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getActivity(),"Opening Camera, this could take a second",Toast.LENGTH_LONG).show();
+                dispatchTakePictureIntent();
+                Toast.makeText(getActivity(),"Opening Camera, this could take a second",Toast.LENGTH_SHORT).show();
             }
         });
 
         detectImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-              Toast.makeText(getActivity(),"deciphering  Text, hold on",Toast.LENGTH_LONG).show();
+                detectTextFromImage();
+                Toast.makeText(getActivity(),"deciphering  Text, hold on",Toast.LENGTH_LONG).show();
             }
         });
 
 
         return fragmentView;
     }
+
+
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity( getActivity().getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+
+        }
+    }
+    private void detectTextFromImage() {
+
+
+
+
+    }
+
 
 }
 
